d31d9630081930d6dbd403f2a914421d1d37fe10
erasels <emanuelbedburdick@yahoo.de>
2019-02-11 16:40:33 +0100

    S.Coin String fix and Neowpatch loc fix

--- src/main/java/aspiration/patches/NeowRewardPatches.java
+++ src/main/java/aspiration/patches/NeowRewardPatches.java
@@ -31,10 +31,11 @@ public class NeowRewardPatches {
                     String tmp;
                     switch (Settings.language) {
                         case RUS:
-                            String bleh ="Получить дополнительную стартовую реликвию";
-                            byte bytes[] = bleh.getBytes(StandardCharsets.UTF_8);
-                            String blah = new String(bytes, StandardCharsets.UTF_8);
-                            tmp = FontHelper.colorString(blah, "g");
+                            String foo = "\u041f\u043e\u043b\u0443\u0447\u0438\u0442\u044c \u0434\u043e\u043f\u043e\u043b\u043d\u0438\u0442\u0435\u043b\u044c\u043d\u0443\u044e \u0441\u0442\u0430\u0440\u0442\u043e\u0432\u0443\u044e \u0440\u0435\u043b\u0438\u043a\u0432\u0438\u044e";
+                            //byte x[] = foo.getBytes(StandardCharsets.UTF_8);
+                            //String y = new String(x, StandardCharsets.UTF_8);
+                            //String blah = new String("\\u041f\\u043e\\u043b\\u0443\\u0447\\u0438\\u0442\\u044c \\u0434\\u043e\\u043f\\u043e\\u043b\\u043d\\u0438\\u0442\\u0435\\u043b\\u044c\\u043d\\u0443\\u044e \\u0441\\u0442\\u0430\\u0440\\u0442\\u043e\\u0432\\u0443\\u044e \\u0440\\u0435\\u043b\\u0438\\u043a\\u0432\\u0438\\u044e".getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8);
+                            tmp = FontHelper.colorString(foo, "g");
                             break;
                         case DEU:
                             tmp = FontHelper.colorString("Obtain additional Starter Relic", "g");
