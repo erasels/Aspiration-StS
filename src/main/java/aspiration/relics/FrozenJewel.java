c7bdfa23220247caaa5b671d3673435f57fef34c
erasels <emanuelbedburdick@yahoo.de>
2019-03-04 17:20:57 +0100

    FrozenJewel VfX added

--- src/main/java/aspiration/relics/FrozenJewel.java
+++ src/main/java/aspiration/relics/FrozenJewel.java
@@ -1,5 +1,8 @@
 package aspiration.relics;
 
+import aspiration.relics.abstracts.AspirationRelic;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
@@ -7,8 +10,7 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.BlurPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
-
-import aspiration.relics.abstracts.AspirationRelic;
+import com.megacrit.cardcrawl.vfx.combat.VerticalAuraEffect;
 
 public class FrozenJewel extends AspirationRelic {
 	public static final String ID = "aspiration:FrozenJewel";
@@ -40,6 +42,7 @@ public class FrozenJewel extends AspirationRelic {
     		}
     	}
     	if(atk_counter > 1 && atk_counter == mon_counter) {
+			AbstractDungeon.actionManager.addToBottom(new VFXAction(AbstractDungeon.player, new VerticalAuraEffect(Color.LIGHT_GRAY, AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY), 0.0F));
     		AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
     		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new BlurPower(AbstractDungeon.player, 1), 1));
     	}
