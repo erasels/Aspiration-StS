aa51542487e0cc72bbca65097557b40b8f81cae1
erasels <emanuelbedburdick@yahoo.de>
2019-09-13 14:48:59 +0200

    Added D6 relic reward and fixed Looter interaction

--- src/main/java/aspiration/patches/NeowRewardPatches.java
+++ src/main/java/aspiration/patches/NeowRewardPatches.java
@@ -1,9 +1,13 @@
 package aspiration.patches;
 
 import aspiration.Aspiration;
+import aspiration.Utility.RelicUtils;
+import aspiration.relics.rare.DSix;
 import aspiration.relics.uncommon.Nostalgia;
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
@@ -19,23 +23,31 @@ public class NeowRewardPatches {
 	//Thanks Reina :D
 	@SpireEnum
     public static NeowReward.NeowRewardType NOSTALGIA;
+    @SpireEnum
+    public static NeowReward.NeowRewardType DSIX;
 	
 	@SpirePatch(
             clz = NeowReward.class,
             method = "getRewardOptions"
     )
     public static class blessAddNostalgia {
-        public static ArrayList<NeowRewardDef> Postfix(ArrayList<NeowRewardDef> __result, NeowReward __instance, final int category) {
+	    @SpirePostfixPatch
+        public static ArrayList<NeowRewardDef> patch(ArrayList<NeowRewardDef> __result, NeowReward __instance, final int category) {
+            if (category == 2 && __instance.drawback != NeowReward.NeowRewardDrawback.CURSE) {
+                String tmp;
+                switch (Settings.language) {
+                    case ZHS:
+                        tmp = FontHelper.colorString("\u66f4\u6539\u6700\u591a\u0036\u4ef6\u9057\u7269\u548c\u836f\u6c34\u5956\u52b1", "g");
+                        break;
+                    default:
+                        tmp = FontHelper.colorString("Reroll up to 6 relic and potion rewards", "g");
+                }
+                __result.add(new NeowRewardDef(DSIX, tmp + " ]"));
+            }
+
             if (category == 3) {
                 String tmp;
                 switch (Settings.language) {
-                    case RUS:
-                        String foo = "\u041f\u043e\u043b\u0443\u0447\u0438\u0442\u044c \u0434\u043e\u043f\u043e\u043b\u043d\u0438\u0442\u0435\u043b\u044c\u043d\u0443\u044e \u0441\u0442\u0430\u0440\u0442\u043e\u0432\u0443\u044e \u0440\u0435\u043b\u0438\u043a\u0432\u0438\u044e";
-                        tmp = FontHelper.colorString(foo, "g");
-                        break;
-                    case DEU:
-                        tmp = FontHelper.colorString("Erhalte ein zusätzliches Anfängerrelikt", "g");
-                        break;
 					case ZHS:
 						tmp = FontHelper.colorString("\u989d\u5916\u83b7\u5f97\u4e00\u4ef6\u521d\u59cb\u9057\u7269", "g");
 						break;
@@ -53,11 +65,16 @@ public class NeowRewardPatches {
             method = "activate"
     )
     public static class ActivatePatch {
-        public static void Prefix(NeowReward __instance) {
+	    @SpirePrefixPatch
+        public static void patch(NeowReward __instance) {
             if (__instance.type == NOSTALGIA) {
                 AbstractDungeon.getCurrRoom().spawnRelicAndObtain(Settings.WIDTH / 3, Settings.HEIGHT / 2, new Nostalgia(Aspiration.uncommonNostalgia()));
                 AbstractDungeon.uncommonRelicPool.removeIf(relic -> relic.equals(Nostalgia.ID));
                 AbstractDungeon.shopRelicPool.removeIf(relic -> relic.equals(Nostalgia.ID));
+            }
+            if (__instance.type == DSIX) {
+                AbstractDungeon.getCurrRoom().spawnRelicAndObtain(Settings.WIDTH / 3, Settings.HEIGHT / 2, new DSix());
+                RelicUtils.removeRelicFromPool(DSix.ID, true);
             }
         }
     }
--- src/main/java/aspiration/patches/Unique/DSixRewardScreenTriggerPatch.java
+++ src/main/java/aspiration/patches/Unique/DSixRewardScreenTriggerPatch.java
@@ -6,13 +6,27 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.screens.CombatRewardScreen;
 
-@SpirePatch(clz = CombatRewardScreen.class, method = "open", paramtypez = {})
 public class DSixRewardScreenTriggerPatch {
-    @SpirePostfixPatch
-    public static void trigger(CombatRewardScreen __instance) {
+    @SpirePatch(clz = CombatRewardScreen.class, method = "open", paramtypez = {})
+    public static class Normal {
+        @SpirePostfixPatch
+        public static void trigger(CombatRewardScreen __instance) {
+            dcheck();
+        }
+    }
+
+    @SpirePatch(clz = CombatRewardScreen.class, method = "openCombat", paramtypez = {String.class, boolean.class})
+    public static class Mugger {
+        @SpirePostfixPatch
+        public static void trigger(CombatRewardScreen __instance, String x, boolean y) {
+            dcheck();
+        }
+    }
+
+    private static void dcheck() {
         DSix d6 = (DSix) AbstractDungeon.player.getRelic(DSix.ID);
-        if(d6 != null) {
-            if(d6.counter > 0 && d6.countRewards() > 0) {
+        if (d6 != null) {
+            if (d6.counter > 0 && d6.countRewards() > 0) {
                 d6.beginLongPulse();
             }
         }
