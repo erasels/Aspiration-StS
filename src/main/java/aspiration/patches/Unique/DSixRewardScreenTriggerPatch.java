71356746efd5618b55f2bdc8b247c833a9ea46ae
erasels <emanuelbedburdick@yahoo.de>
2019-09-14 11:44:50 +0200

    Made D6 compatible with Callingbell

--- src/main/java/aspiration/patches/Unique/DSixRewardScreenTriggerPatch.java
+++ src/main/java/aspiration/patches/Unique/DSixRewardScreenTriggerPatch.java
@@ -1,10 +1,16 @@
 package aspiration.patches.Unique;
 
 import aspiration.relics.rare.DSix;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.CallingBell;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import com.megacrit.cardcrawl.screens.CombatRewardScreen;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+
+import java.util.ArrayList;
 
 public class DSixRewardScreenTriggerPatch {
     @SpirePatch(clz = CombatRewardScreen.class, method = "open", paramtypez = {})
@@ -20,6 +26,21 @@ public class DSixRewardScreenTriggerPatch {
         @SpirePostfixPatch
         public static void trigger(CombatRewardScreen __instance, String x, boolean y) {
             dcheck();
+        }
+    }
+
+    @SpirePatch(clz = CallingBell.class, method = "update")
+    public static class CallingbellCompat {
+        @SpireInsertPatch(locator = Locator.class)
+        public static void patch(CallingBell __instance) {
+            dcheck();
+        }
+
+        private static class Locator extends SpireInsertLocator {
+            public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+                Matcher finalMatcher = new Matcher.FieldAccessMatcher(AbstractRoom.class, "rewardPopOutTimer");
+                return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher);
+            }
         }
     }
 
