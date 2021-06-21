76296593a4119a1ecef64e37c487934b6dd2c221
erasels <emanuelbedburdick@yahoo.de>
2021-06-21 22:38:21 +0200

    Ritual dagger string fix

--- src/main/java/aspiration/relics/boss/RitualDagger.java
+++ src/main/java/aspiration/relics/boss/RitualDagger.java
@@ -35,7 +35,6 @@ public class RitualDagger extends AspirationRelic implements CustomSavable<Boole
 
     public RitualDagger() {
         super(ID, "RitualDagger.png", RelicTier.BOSS, LandingSound.SOLID);
-        this.initializeTips();
     }
 
     @Override
