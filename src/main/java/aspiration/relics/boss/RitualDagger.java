a2837a5fb5c94f7fdc035beeff896e992ae56070
erasels <emanuelbedburdick@yahoo.de>
2021-06-22 13:55:51 +0200

    Reverted RitualDagger rework

--- src/main/java/aspiration/relics/boss/RitualDagger.java
+++ src/main/java/aspiration/relics/boss/RitualDagger.java
@@ -1,6 +1,7 @@
 package aspiration.relics.boss;
 
 import aspiration.Aspiration;
+import aspiration.powers.AwokenPower;
 import aspiration.relics.abstracts.AspirationRelic;
 import basemod.abstracts.CustomSavable;
 import com.badlogic.gdx.Gdx;
@@ -8,6 +9,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.MathUtils;
 import com.evacipated.cardcrawl.mod.stslib.relics.OnPlayerDeathRelic;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.LoseHPAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.unique.RemoveDebuffsAction;
@@ -16,6 +18,7 @@ import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.helpers.PowerTip;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.relics.MarkOfTheBloom;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
@@ -27,7 +30,6 @@ public class RitualDagger extends AspirationRelic implements CustomSavable<Boole
 	public static final String ID = "aspiration:RitualDagger";
 	
     private static final int HP_LOSS = 3;
-	private static final int HP_GAIN = 13;
 	private static final float COOLDOWN_AMT = 0.2F;
     private float cooldown = COOLDOWN_AMT;
     
@@ -35,11 +37,15 @@ public class RitualDagger extends AspirationRelic implements CustomSavable<Boole
 
     public RitualDagger() {
         super(ID, "RitualDagger.png", RelicTier.BOSS, LandingSound.SOLID);
+        this.tips.clear();
+        this.tips.add(new PowerTip(name, description));
+        this.tips.add(new PowerTip(AwokenPower.NAME, AwokenPower.getDesc()));
+        this.initializeTips();
     }
 
     @Override
     public String getUpdatedDescription() {
-        return String.format(DESCRIPTIONS[0], HP_LOSS, HP_GAIN);
+        return DESCRIPTIONS[0] + HP_LOSS + DESCRIPTIONS[1] + DESCRIPTIONS[2];
     }
 
 	@Override
@@ -59,6 +65,8 @@ public class RitualDagger extends AspirationRelic implements CustomSavable<Boole
 			AbstractDungeon.actionManager.addToBottom(new SFXAction("ATTACK_PIERCING_WAIL"));
 			AbstractDungeon.actionManager.addToBottom(new RemoveDebuffsAction(AbstractDungeon.player));
 			AbstractDungeon.actionManager.addToBottom(new VFXAction(AbstractDungeon.player, new RoomTintEffect(Color.FIREBRICK, 0.3f, 999f, true), 0.0F));
+
+			AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new AwokenPower(AbstractDungeon.player), 1));
 			return false;
 		}
     	return true;
@@ -73,22 +81,23 @@ public class RitualDagger extends AspirationRelic implements CustomSavable<Boole
     public void atTurnStartPostDraw()
     {
     	flash();
+		if (usedUp) {
+			AbstractDungeon.actionManager.addToBottom(new VFXAction(AbstractDungeon.player, new VerticalAuraEffect(Color.FIREBRICK, AbstractDungeon.player.hb.cX, AbstractDungeon.player.hb.cY), 0.0F));
+			AbstractDungeon.actionManager.addToBottom(new SFXAction("MONSTER_JAW_WORM_BELLOW"));
+		}
     	AbstractDungeon.actionManager.addToBottom(new LoseHPAction(AbstractDungeon.player, AbstractDungeon.player, HP_LOSS));
       
     }
+    
+    @Override
+    public void atPreBattle() {
+    	if(usedUp) {
+    		AbstractDungeon.actionManager.addToTop(new VFXAction(AbstractDungeon.player, new RoomTintEffect(Color.FIREBRICK, 0.3f, 999f, true), 0.0F));
+    		AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new AwokenPower(AbstractDungeon.player), 1));
+    	}
+    }
 
-	@Override
-	public void onVictory() {
-		if(usedUp) {
-			flash();
-			AbstractPlayer p = AbstractDungeon.player;
-			addToTop(new RelicAboveCreatureAction(p, this));
-			if (p.currentHealth > 0)
-				p.heal(12);
-		}
-	}
-
-	@Override
+    @Override
 	public void update() {
 		if(usedUp) {
 			cooldown -= Gdx.graphics.getDeltaTime();
