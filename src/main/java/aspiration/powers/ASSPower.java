8bf84b16a334828de40196ce719925dcd0a5cd7a
erasels <emanuelbedburdick@yahoo.de>
2022-02-05 13:53:26 +0100

    Rework ASS

---
+++ src/main/java/aspiration/powers/ASSPower.java
@@ -0,0 +1,33 @@
+package aspiration.powers;
+
+import aspiration.powers.abstracts.AspirationPower;
+import com.badlogic.gdx.graphics.Color;
+import com.evacipated.cardcrawl.mod.stslib.patches.NeutralPowertypePatch;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.HealthBarRenderPower;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.InvisiblePower;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+public class ASSPower extends AspirationPower implements InvisiblePower, HealthBarRenderPower {
+    public static final String POWER_ID = "aspiration:ASSPower";
+    private AbstractRelic reference;
+
+    public ASSPower(AbstractRelic ASS) {
+        this.name = "";
+        this.ID = POWER_ID;
+        this.owner = AbstractDungeon.player;
+        this.type = NeutralPowertypePatch.NEUTRAL;
+        reference = ASS;
+    }
+
+
+    @Override
+    public int getHealthBarAmount() {
+        return reference.counter;
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.WHITE;
+    }
+}
--- src/main/java/aspiration/relics/rare/AnachronicSnailShell.java
+++ src/main/java/aspiration/relics/rare/AnachronicSnailShell.java
@@ -1,25 +1,31 @@
 package aspiration.relics.rare;
 
+import aspiration.powers.ASSPower;
 import aspiration.relics.abstracts.AspirationRelic;
-import com.badlogic.gdx.math.MathUtils;
+import basemod.ReflectionHacks;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.mod.stslib.relics.BetterOnLoseHpRelic;
 import com.megacrit.cardcrawl.actions.AbstractGameAction.AttackEffect;
 import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.SFXAction;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.DamageInfo.DamageType;
+import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.powers.TimeWarpPower;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.vfx.combat.FlashPowerEffect;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class AnachronicSnailShell extends AspirationRelic implements BetterOnLoseHpRelic {
 	public static final String ID = "aspiration:AnachronicSnailShell";
 	
-	private static final int START_CHARGE = 0;
-	private boolean duringTurn = true;
+	private static final int THRESHOLD = 6;
 
     public AnachronicSnailShell() {
         super(ID, "AnachronicSnailShell.png", RelicTier.RARE, LandingSound.CLINK);
@@ -27,77 +33,59 @@ public class AnachronicSnailShell extends AspirationRelic implements BetterOnLos
 
     @Override
     public String getUpdatedDescription() {
-        return DESCRIPTIONS[0];
+        return String.format(DESCRIPTIONS[0], THRESHOLD);
     }
 
-    /*@Override
-    public int onAttacked(DamageInfo info, int damageAmount) {
-    	if ((AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) && (damageAmount > 0) && (AbstractDungeon.player.currentBlock < damageAmount) && !(AbstractDungeon.player.endTurnQueued) && info.owner != AbstractDungeon.player) {
-    		flash();
-            AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
-            AbstractDungeon.actionManager.addToBottom(new SFXAction("POWER_TIME_WARP"));
-            pulse = true;
-            
-            manipCharge(damageAmount - AbstractDungeon.player.currentBlock);
-            return 0;
-    	}
-    	return damageAmount;
-    }*/
+    @Override
+    public void atBattleStart() {
+        addToBot(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new ASSPower(this)));
+        setCounter(0);
+    }
 
     @Override
     public int betterOnLoseHp(DamageInfo info, int Amount) {
-        if ((AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) && !duringTurn && info.owner != AbstractDungeon.player && info.type == DamageType.NORMAL && Amount > 1) {
+        if (info.owner != AbstractDungeon.player && info.owner != null && AbstractDungeon.actionManager.turnHasEnded && info.type == DamageType.NORMAL && Amount > 0 && counter < THRESHOLD) {
             flash();
             AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
             AbstractDungeon.actionManager.addToBottom(new SFXAction("POWER_TIME_WARP"));
             beginLongPulse();
 
-            int tmp = MathUtils.floor((float)Amount /2);
-            manipCharge(tmp);
+            int tmp = NumberUtils.min(Amount, THRESHOLD - counter);
+            counter += tmp;
             return Amount - tmp;
         }
         return Amount;
     }
 
     @Override
-    public void onPlayerEndTurn()
-    {
-        duringTurn = false;
+    public void onPlayerEndTurn() {
     	if(counter > 0) {
     		//AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, new DamageInfo(AbstractDungeon.getMonsters().getRandomMonster(true), counter, DamageType.NORMAL), AttackEffect.BLUNT_LIGHT));
             AbstractDungeon.actionManager.addToBottom(new VFXAction(AbstractDungeon.player, new FlashPowerEffect(new TimeWarpPower(AbstractDungeon.player)), 0.0F));
             AbstractDungeon.actionManager.addToBottom(new DamageAction(AbstractDungeon.player, new DamageInfo(AbstractDungeon.getMonsters().getRandomMonster(true), counter, DamageType.THORNS), AttackEffect.BLUNT_LIGHT));
-    		startingCharges();
+    		setCounter(0);
     		stopPulse();
     	}
-    }
-
-    @Override
-    public void atTurnStart() {
-        duringTurn = true;
     }
     
     @Override
     public void onVictory() {
-    	startingCharges();
+    	setCounter(-1);
     	stopPulse();
     }
-    
+
     @Override
-    public void onEquip()
-    {
-        startingCharges();
-    }
-    
-    private void startingCharges()
-    {
-        setCounter(START_CHARGE);
-    }
-    
-    private void manipCharge(int amt) {
-        if (counter < 0) {
-            counter = 0;
+    public void renderCounter(SpriteBatch sb, boolean inTopPanel) {
+        if (this.counter > -1) {
+            if (inTopPanel) {
+                Color c = Color.WHITE;
+                if(counter >= THRESHOLD) {
+                    c = Color.SALMON;
+                }
+                FontHelper.renderFontRightTopAligned(sb, FontHelper.topPanelInfoFont, counter + "/" + THRESHOLD, ((float)ReflectionHacks.getPrivateStatic(AbstractRelic.class, "offsetX")) + currentX + 30.0F * Settings.scale, currentY - 7.0F * Settings.scale, c);
+            } else {
+                FontHelper.renderFontRightTopAligned(sb, FontHelper.topPanelInfoFont, Integer.toString(counter), currentX + 30.0F * Settings.scale, currentY - 7.0F * Settings.scale, Color.WHITE);
+            }
         }
-        setCounter(counter + amt);
     }
 }
